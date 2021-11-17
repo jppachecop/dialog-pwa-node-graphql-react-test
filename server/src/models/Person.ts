72272af66612bef4cfc3a7367ed198ad93e73736
João Paulo Pacheco <jppacheco09@hotmail.com>
2021-11-16 22:15:53 -0300

    fiz: remoção do typeorm

--- server/src/models/Person.ts
+++ server/src/models/Person.ts
@@ -1,42 +1,31 @@
 import { Field, ID, Int, ObjectType } from "type-graphql";
-import { Column, Entity } from "typeorm";
 
-@Entity()
 @ObjectType()
 export class Person {
   @Field(() => ID)
-  @Column()
   _id!: string;
 
   @Field(() => Int)
-  @Column()
   index!: number;
 
   @Field()
-  @Column()
   picture: string = "";
 
   @Field(() => Int)
-  @Column()
   age!: number;
 
   @Field()
-  @Column()
   eyeColor!: string;
 
   @Field()
-  @Column()
   name!: string;
 
   @Field()
-  @Column()
   company!: string;
 
   @Field()
-  @Column()
   email!: string;
 
   @Field()
-  @Column()
   phone!: string;
 }
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,12 +1,9 @@
 import { Field, ObjectType } from "type-graphql";
-import { Column, Entity } from "typeorm";
 import { Person } from "./Person";
 
-@Entity()
 @ObjectType()
 export class User extends Person {
   @Field()
-  @Column()
   greeting: string = "You are up to date with your messages ;)";
 
   @Field(() => [Person])
